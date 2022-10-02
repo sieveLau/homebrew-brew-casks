e1a07c71d05f74b92f08908b6f078a9cd41af7d8
Sieve Lau <sievelau@gmail.com>
2022-10-02 22:24:18 +0800

    clion

---
+++ clion.rb
@@ -0,0 +1,45 @@
+cask "clion" do
+  arch arm: "-aarch64"
+
+  version "2022.2.3,222.4167.35"
+  sha256 arm:   "d921c83ee6b444520e7fdb9fdae4ec0e67cea6dfcaa720e6b36d85c17a4df39c",
+         intel: "d7e0dcbb3c28f383d1365f5cb6d2b55afbb8c638fd9f100b8040f72b394a1aed"
+
+  url "https://download.jetbrains.com/cpp/CLion-#{version.csv.first}#{arch}.dmg"
+  name "CLion"
+  desc "C and C++ IDE"
+  homepage "https://www.jetbrains.com/clion/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=CL&latest=true&type=release"
+    strategy :page_match do |page|
+      JSON.parse(page)["CL"].map do |release|
+        "#{release["version"]},#{release["build"]}"
+      end
+    end
+  end
+
+  auto_updates false
+  depends_on macos: ">= :high_sierra"
+
+  app "CLion.app"
+
+  uninstall_postflight do
+    ENV["PATH"].split(File::PATH_SEPARATOR).map { |path| File.join(path, "clion") }.each do |path|
+      if File.readable?(path) &&
+         File.readlines(path).grep(/# see com.intellij.idea.SocketLock for the server side of this interface/).any?
+        File.delete(path)
+      end
+    end
+  end
+
+  zap trash: [
+    "~/Library/Application Support/JetBrains/CLion#{version.major_minor}",
+    "~/Library/Caches/JetBrains/CLion#{version.major_minor}",
+    "~/Library/Logs/JetBrains/CLion#{version.major_minor}",
+    "~/Library/Preferences/CLion#{version.major_minor}",
+    "~/Library/Preferences/com.jetbrains.CLion.plist",
+    "~/Library/Preferences/jetbrains.clion.*.plist",
+    "~/Library/Saved Application State/com.jetbrains.CLion.savedState",
+  ]
+end
